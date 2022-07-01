47a2862c4f61dd489ce8dbe7b1881660ca037f44
Adrian Rogowski <adrianrogowski80@gmail.com>
2022-07-02 00:54:57 +0200

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,118 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <time-number v-for="(item, index) in time" :key="index" :index="index">
+      <template v-slot:label>{{ item.label }}</template>
+      {{ item.value }}
+    </time-number>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TimeNumber from "./components/TimeNumber.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: { TimeNumber },
+  name: "App",
+  data() {
+    return {
+      endDate: new Date(),
+      currentDate: new Date().getHours,
+      time: {
+        d: { label: "Days", value: 0 },
+        h: { label: "Hours", value: 0 },
+        m: { label: "Minutes", value: 0 },
+        s: { label: "Seconds", value: 0 },
+      },
+    };
+  },
+  mounted() {
+    this.getDataBase();
+    this.updateDate();
+  },
+  computed: {
+    day() {
+      const d = Math.floor(
+        (this.endDate - this.currentDate) / (1000 * 60 * 60 * 24)
+      );
+      if (d <= 0) {
+        return "00";
+      }
+      return d > 9 ? d : "0" + d;
+    },
+    hour() {
+      const h =
+        Math.floor((this.endDate - this.currentDate) / (1000 * 60 * 60)) % 24;
+      if (h <= 0) {
+        return "00";
+      }
+      return h > 9 ? h : "0" + h;
+    },
+    minute() {
+      const m =
+        Math.floor((this.endDate - this.currentDate) / (1000 * 60)) % 60;
+      if (m <= 0) {
+        return "00";
+      }
+      return m > 9 ? m : "0" + m;
+    },
+    second() {
+      const s = Math.floor((this.endDate - this.currentDate) / 1000) % 60;
+      if (s <= 0) {
+        return "00";
+      }
+      return s > 9 ? s : "0" + s;
+    },
+  },
+  methods: {
+    updateDate() {
+      this.currentDate = new Date();
+      this.time.d.value = this.day;
+      this.time.h.value = this.hour;
+      this.time.m.value = this.minute;
+      this.time.s.value = this.second;
+      setTimeout(() => {
+        this.updateDate();
+        this.getDataBase();
+      }, 1000);
+    },
+    getDataBase() {
+      fetch(
+        "https://vue-http-project-eb74d-default-rtdb.europe-west1.firebasedatabase.app/EndDate.json"
+      )
+        .then((result) => {
+          return result.json();
+        })
+        .then((data) => {
+          for (const id in data) {
+            this.endDate = new Date(data[id].EndDate);
+          }
+        });
+    },
+  },
+};
 </script>
 
 <style>
+* {
+  margin: 0;
+}
+
+@import url("https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,600;1,500&display=swap");
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-image: url("./assets/wp.png");
+  background-size: cover;
+  width: 100%;
+  height: 100vh;
+  display: grid;
+  align-content: center;
+}
+
+.container {
+  display: grid;
+  width: 37%;
+  grid-template-columns: 1fr 0.1fr 1fr 0.1fr 1fr 0.1fr 1fr;
+  margin-left: 16%;
 }
 </style>
+
---
+++ src/components/TimeNumber.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="digit">
+    <p class="label"><slot name="label"></slot></p>
+    <p class="number"><slot></slot></p>
+  </div>
+  <div class="sepparator" v-if="index !== 's'">
+    <p>:</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["index"],
+};
+</script>
+
+<style scoped>
+.digit {
+  display: grid;
+  text-align: center;
+  font-size: 4rem;
+  grid-template-rows: 0.5fr 2fr;
+}
+.label {
+  color: #142b44;
+  font-family: "Barlow", sans-serif;
+  font-weight: 500;
+  font-size: 1.563rem;
+}
+.number {
+  font-size: 5.938rem;
+  font-family: "Barlow", sans-serif;
+  font-weight: 600;
+  color: #142b44;
+}
+.sepparator {
+  font-size: 5.938rem;
+  font-family: "Barlow", sans-serif;
+  font-weight: 600;
+  color: #142b44;
+  display: grid;
+  grid-template-rows: 0.5fr 2fr;
+}
+.sepparator p {
+  grid-row-start: 2;
+}
+</style>
